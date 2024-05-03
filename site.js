6380295beea2bba0c7b8ac81486cbbe9283da361
Salt_For_Sale <elee823@gmail.com>
2024-05-03 14:58:32 -0400

    step 3 done

--- site.js
+++ site.js
@@ -34,7 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
+            title: "IMDB + Evan's Top 8 Movies",
+            owner: 'Evan Lee',
+            github: 'https://github.com/SaltForSale'
       }
     },
       methods: {
